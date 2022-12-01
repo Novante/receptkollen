4a5230215b8fac6e2278e597e3f9703458607c63
Novante <78790665+Novante@users.noreply.github.com>
2022-12-01 22:16:36 +0100

    Added conversion of units.

--- backend/controller/recipeController.js
+++ backend/controller/recipeController.js
@@ -32,20 +32,54 @@ const checkRecipeSite = (url) => {
 const getIngredientsFromRecept = async () => {
     await page.goto(`https://recept.se/recept/kramig-kycklinggryta-med-svamp-och-bacon`)
 
-        if (await page.$('#onetrust-accept-btn-handler') !== null){
-            await page.click('#onetrust-accept-btn-handler')
-        }
+    if (await page.$('#onetrust-accept-btn-handler') !== null) {
+        await page.click('#onetrust-accept-btn-handler')
+    }
 
-
-
-    console.log(await page.$$eval('.recipe-page-body__ingredients', options => {
+    let ingredientList = (await page.$$eval('.recipe-page-body__ingredients', options => {
         return options.map(option => option.textContent)
     }))
+
+    let filteredList = ingredientList.map(a => a.replace('–', ''))
+    filteredList = filteredList.map(a => a.trim())
+
+    convertToMetric(filteredList)
+
+
 }
 
+const convertToMetric = (filteredList) => {
+    let filteredIngredientArr = [];
+    for (let i = 0; i < filteredList.length; i++) {
+        let ingredientArr = filteredList[i].split(' ')
 
+        let index = ingredientArr.indexOf('')
+        if (index !== -1) {
+            ingredientArr.splice(index, 1)
+        }
 
+        switch (ingredientArr[1]) {
+            case "msk":
+                ingredientArr[0] = ingredientArr[0] * 15
+                ingredientArr[1] = 'g'
+                filteredIngredientArr.push(ingredientArr)
+                break;
+            case "tsk":
+                ingredientArr[0] = ingredientArr[0] * 5
+                ingredientArr[1] = 'g'
+                filteredIngredientArr.push(ingredientArr)
+                break;
+            case "krm":
+                ingredientArr[0] = ingredientArr[0] * 1
+                ingredientArr[1] = 'g'
+                break;
+            default:
+                filteredIngredientArr.push(ingredientArr)
+        }
+    }
 
+    console.log(filteredIngredientArr)
 
+}
 
 export default recipeController;
